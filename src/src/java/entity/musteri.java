774a6c3f4abfac3ea398be98ab2d4b299c364ccb
oguzhance <ockhss@gmail.com>
2022-04-15 15:54:21 +0300

    musteri entity

---
+++ src/src/java/entity/musteri.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Oguzhan
+ */
+public class musteri {
+    private int id;
+    private String isim;
+    private String soyisim;
+    private String tlf;
+
+    public musteri() {
+    }
+
+    public musteri(int id, String isim) {
+        this.id = id;
+        this.isim = isim;
+    }
+
+    public musteri(int id, String isim, String soyisim, String tlf) {
+        this.id = id;
+        this.isim = isim;
+        this.soyisim = soyisim;
+        this.tlf = tlf;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public String getSoyisim() {
+        return soyisim;
+    }
+
+    public void setSoyisim(String soyisim) {
+        this.soyisim = soyisim;
+    }
+
+    public String getTlf() {
+        return tlf;
+    }
+
+    public void setTlf(String tlf) {
+        this.tlf = tlf;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 17 * hash + this.id;
+        hash = 17 * hash + Objects.hashCode(this.isim);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final musteri other = (musteri) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return Objects.equals(this.isim, other.isim);
+    }
+    
+    
+    
+}
